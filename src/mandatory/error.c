065d7f377f279f046d59a30d1da126a260cb1c7e
sideseal <gychoi@student.42seoul.kr>
2023-06-10 14:10:33 +0900

    gychoi: add leak test

--- src/mandatory/error.c
+++ src/mandatory/error.c
@@ -6,17 +6,24 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 23:05:38 by gychoi            #+#    #+#             */
-/*   Updated: 2023/06/08 21:16:29 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/06/10 14:10:09 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
+
+void	check_leaks(void)
+{
+	// 지우는거 잊지 말기
+	system("leaks --list -- miniRT");
+}
 
 void	print_custom_error(char *msg, void *data)
 {
 	printf("Error\n");
 	printf("%s\n", msg);
 	free_struct(data);
+	atexit(check_leaks);
 	exit(1);
 }
 
@@ -25,6 +32,7 @@ void	print_error(char *msg, void *data)
 	printf("Error\n");
 	perror(msg);
 	free_struct(data);
+	atexit(check_leaks);
 	exit(1);
 }
 
@@ -37,5 +45,7 @@ void	print_read_error(char *msg, char *loc, void *data, char **tokens)
 	printf("%s\n", msg);
 	free_tokens(tokens);
 	free_struct(data);
+	atexit(check_leaks);
 	exit(1);
 }
+
--- src/mandatory/hook.c
+++ src/mandatory/hook.c
@@ -6,15 +6,23 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/02 20:20:33 by gychoi            #+#    #+#             */
-/*   Updated: 2023/06/08 21:37:30 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/06/10 14:09:52 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+static void	check_leaks(void)
+{
+	// 지우는거 잊지 말기
+	system("leaks --list -- miniRT");
+}
+
 int	exit_hook(t_vars *vars)
 {
+	//mlx_destroy_window(vars->mlx, vars->win);
 	free_struct(vars);
+	atexit(check_leaks);
 	exit(0);
 }
 
@@ -22,7 +30,9 @@ int	key_hook(int keycode, t_vars *vars)
 {
 	if (keycode == ESC)
 	{
+		//mlx_destroy_window(vars->mlx, vars->win);
 		free_struct(vars);
+		atexit(check_leaks);
 		exit(0);
 	}
 	return (0);
