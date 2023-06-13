6199f4e3ecd3232449ba7f5a3a57f8f002e0538e
sideseal <gychoi@student.42seoul.kr>
2023-06-13 14:55:27 +0900

    gychoi: hotfix: modify is_error

--- src/mandatory/set_utils.c
+++ src/mandatory/set_utils.c
@@ -6,7 +6,7 @@
 /*   By: gychoi <gychoi@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 22:27:41 by gychoi            #+#    #+#             */
-/*   Updated: 2023/06/11 19:33:10 by gychoi           ###   ########.fr       */
+/*   Updated: 2023/06/13 14:54:23 by gychoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,39 @@
 
 t_bool	set_vars_value(char *elem, void *value, int type)
 {
-	int		error;
+	t_bool	is_error;
 
-	error = FALSE;
+	is_error = FALSE;
 	if (type == D_FLOAT)
-		*(double *)value = rt_atof(elem, &error);
+		*(double *)value = rt_atof(elem, &is_error);
 	else if (type == D_INT)
 		*(int *)value = ft_atoi(elem);
 	else if (type == P_RADIUS)
-		*(double *)value = rt_atof(elem, &error) / 2.0;
+		*(double *)value = rt_atof(elem, &is_error) / 2.0;
 	else
-		error = TRUE;
-	return (!error);
+		is_error = TRUE;
+	return (!is_error);
 }
 
 t_bool	set_vars_csv(char *elem, void *csv, int type_s)
 {
-	int		err;
+	t_bool	is_error;
 	char	**tokens;
 
-	err = FALSE;
+	is_error = FALSE;
 	tokens = ft_split(elem, ',');
 	if (type_s == S_COLOR)
-		*(t_point3 *)csv = color3_(rt_atof(tokens[0], &err) / 255, \
-			rt_atof(tokens[1], &err) / 255, rt_atof(tokens[2], &err) / 255);
+		*(t_point3 *)csv = color3_(rt_atof(tokens[0], &is_error) / 255, \
+			rt_atof(tokens[1], &is_error) / 255, \
+			rt_atof(tokens[2], &is_error) / 255);
 	else if (type_s == S_POINT)
-		*(t_point3 *)csv = point3_(rt_atof(tokens[0], &err), \
-			rt_atof(tokens[1], &err), rt_atof(tokens[2], &err));
+		*(t_point3 *)csv = point3_(rt_atof(tokens[0], &is_error), \
+			rt_atof(tokens[1], &is_error), rt_atof(tokens[2], &is_error));
 	else if (type_s == S_VEC)
-		*(t_vec3 *)csv = vec3_(rt_atof(tokens[0], &err), \
-			rt_atof(tokens[1], &err), rt_atof(tokens[2], &err));
+		*(t_vec3 *)csv = vec3_(rt_atof(tokens[0], &is_error), \
+			rt_atof(tokens[1], &is_error), rt_atof(tokens[2], &is_error));
 	else
-		err = TRUE;
+		is_error = TRUE;
 	free_tokens(tokens);
-	return (!err);
+	return (!is_error);
 }
